0283fb4129a6024c58356fed4f035d09ef5ac04e
Gautham S <42068013+myth207@users.noreply.github.com>
2020-03-30 14:44:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,14 +17,19 @@ class MyWindow(QWidget):
         label = QLabel(self.tr("press Enter to display network statistics"))
         self.le = QLineEdit()
         self.te = QTextEdit()
+	self.btn = QPushButton("Exit")
         layout = QVBoxLayout(self)
         layout.addWidget(label)
         layout.addWidget(self.le)
         layout.addWidget(self.te)
+	layout.addWidget(self.btn)
         self.setLayout(layout) 
 	self.le.setText('location of information : /proc/net/netstat')
         self.connect(self.le,SIGNAL("returnPressed(void)"),
                      self.run_command)
+	self.btn.clicked.connect(quit)
+	
+	
 
     def run_command(self):
         cmd = '''cat /proc/net/netstat |awk '(f==0) { i=1; while ( i<=NF) {n[i] = $i; i++ }; f=1; 	next}(f==1){ i=2; while ( i<=NF){ printf "%s = %d\\n", n[i], $i; i++}; f=0} ' '''
